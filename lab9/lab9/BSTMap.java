2b1a9ccf810cdaa18fbc641b2a733deebfdfb1d4
Tinuvile <3145593233@qq.com>
2024-11-27 17:04:01 +0800

    Lab9

--- lab9/lab9/BSTMap.java
+++ lab9/lab9/BSTMap.java
@@ -1,12 +1,14 @@
 package lab9;
 
 import java.util.Iterator;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 /**
  * Implementation of interface Map61B with BST as core data structure.
  *
- * @author Your name here
+ * @author 张竹和
  */
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -40,40 +42,74 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
         size = 0;
     }
 
-    /** Returns the value mapped to by KEY in the subtree rooted in P.
-     *  or null if this map contains no mapping for the key.
+    /**
+     * Returns the value mapped to by KEY in the subtree rooted in P.
+     * or null if this map contains no mapping for the key.
      */
     private V getHelper(K key, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            return getHelper(key, p.left);
+        } else if (cmp > 0) {
+            return getHelper(key, p.right);
+        } else {
+            return p.value;
+        }
+        // throw new UnsupportedOperationException();
     }
 
-    /** Returns the value to which the specified key is mapped, or null if this
-     *  map contains no mapping for the key.
+    /**
+     * Returns the value to which the specified key is mapped, or null if this
+     * map contains no mapping for the key.
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        return getHelper(key, root);
+        // throw new UnsupportedOperationException();
     }
 
-    /** Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
-      * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
+    /**
+     * Returns a BSTMap rooted in p with (KEY, VALUE) added as a key-value mapping.
+     * Or if p is null, it returns a one node BSTMap containing (KEY, VALUE).
      */
     private Node putHelper(K key, V value, Node p) {
-        throw new UnsupportedOperationException();
+        if (p == null) {
+            return new Node(key, value);
+        }
+
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            p.left = putHelper(key, value, p.left);
+        } else if (cmp > 0) {
+            p.right = putHelper(key, value, p.right);
+        } else {
+            p.value = value;
+        }
+
+        return p;
+        // throw new UnsupportedOperationException();
     }
 
-    /** Inserts the key KEY
-     *  If it is already present, updates value to be VALUE.
+    /**
+     * Inserts the key KEY
+     * If it is already present, updates value to be VALUE.
      */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        root = putHelper(key, value, root);
+        size++;
+        // throw new UnsupportedOperationException();
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return this.size;
+        // throw new UnsupportedOperationException();
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -81,29 +117,121 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keys = new HashSet<>();
+        fillKeySet(root, keys);
+        return keys;
+        // throw new UnsupportedOperationException();
     }
 
-    /** Removes KEY from the tree if present
-     *  returns VALUE removed,
-     *  null on failed removal.
+    private void fillKeySet(Node p, Set<K> keys) {
+        if (p != null) {
+            fillKeySet(p.left, keys);
+            keys.add(p.key);
+            fillKeySet(p.right, keys);
+        }
+    }
+
+    /**
+     * Removes KEY from the tree if present
+     * returns VALUE removed,
+     * null on failed removal.
      */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V[] removedValue = (V[]) new Object[1];  // 使用数组来保存被移除的值
+        root = removeHelper(key, root, removedValue);  // 调用辅助方法
+        return removedValue[0];  // 返回被移除的值
+        // throw new UnsupportedOperationException();
     }
 
-    /** Removes the key-value entry for the specified key only if it is
-     *  currently mapped to the specified value.  Returns the VALUE removed,
-     *  null on failed removal.
+    /**
+     * 辅助方法，递归地从以 p 为根的子树中移除 KEY。
+     * 同时返回被移除的值。
+     */
+    private Node removeHelper(K key, Node p, V[] removedValue) {
+        if (p == null) {
+            return null;  // 找不到该键
+        }
+
+        int cmp = key.compareTo(p.key);
+        if (cmp < 0) {
+            // 在左子树中查找并删除
+            p.left = removeHelper(key, p.left, removedValue);
+        } else if (cmp > 0) {
+            // 在右子树中查找并删除
+            p.right = removeHelper(key, p.right, removedValue);
+        } else {
+            // 找到要删除的节点
+            removedValue[0] = p.value;  // 保存被移除的值
+            // 处理删除操作
+            if (p.left == null) {
+                return p.right;  // 只有右子树
+            } else if (p.right == null) {
+                return p.left;  // 只有左子树
+            } else {
+                // 有两个子树的情况
+                Node successor = findMin(p.right);  // 找到右子树的最小节点
+                p.key = successor.key;  // 用最小节点的键替换当前节点的键
+                p.value = successor.value;  // 用最小节点的值替换当前节点的值
+                p.right = removeHelper(successor.key, p.right, removedValue);  // 删除最小节点
+            }
+        }
+        return p;  // 返回当前节点
+    }
+
+    /**
+     * 辅助方法，找到以 p 为根的子树中的最小节点。
+     */
+    private Node findMin(Node p) {
+        while (p.left != null) {
+            p = p.left;  // 一直向左查找
+        }
+        return p;  // 返回最小节点
+    }
+
+    /**
+     * Removes the key-value entry for the specified key only if it is
+     * currently mapped to the specified value.  Returns the VALUE removed,
+     * null on failed removal.
      **/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (containsKey(key) && get(key).equals(value)) {
+            return remove(key);  // 如果键存在且值匹配，调用单一的 remove 方法
+        }
+        return null;  // 否则返回 null
+        // throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator<K>() {
+            private Stack<Node> stack = new Stack<>();  // 使用栈来模拟递归
+            private Node current = root;
+
+            { // 初始化时将左子树的所有节点推入栈
+                pushLeft(current);
+            }
+
+            private void pushLeft(Node node) {
+                while (node != null) {
+                    stack.push(node);
+                    node = node.left;  // 一直向左推
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                return !stack.isEmpty();  // 栈不为空时还有下一个元素
+            }
+
+            @Override
+            public K next() {
+                Node node = stack.pop();  // 弹出栈顶元素
+                pushLeft(node.right);  // 将右子树的所有左节点推入栈
+                return node.key;  // 返回当前节点的键
+            }
+        };
+        // throw new UnsupportedOperationException();
     }
 }
--- lab9/lab9/MyHashMap.java
+++ lab9/lab9/MyHashMap.java
@@ -1,13 +1,15 @@
 package lab9;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 /**
  *  A hash table-backed Map implementation. Provides amortized constant time
  *  access to elements via get(), remove(), and put() in the best case.
  *
- *  @author Your name here
+ *  @author 张竹和
  */
 public class MyHashMap<K, V> implements Map61B<K, V> {
 
@@ -53,19 +55,53 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      */
     @Override
     public V get(K key) {
-        throw new UnsupportedOperationException();
+        int index = hash(key);
+        if (buckets[index] == null) {
+            return null;
+        }
+        return buckets[index].get(key);
+        // throw new UnsupportedOperationException();
     }
 
     /* Associates the specified value with the specified key in this map. */
     @Override
     public void put(K key, V value) {
-        throw new UnsupportedOperationException();
+        if (loadFactor() > MAX_LF) {
+            resize(buckets.length * 2);
+        }
+
+        int index = hash(key);
+        if (buckets[index] == null) {
+            buckets[index] = new ArrayMap<>();
+        }
+
+        buckets[index].put(key, value);
+        size++;
+        // throw new UnsupportedOperationException();
+    }
+
+    private void resize(int newCapacity) {
+        ArrayMap<K, V>[] newBuckets = new ArrayMap[newCapacity];
+        for (ArrayMap<K, V> bucket : buckets) {
+            if (bucket != null) {
+                for (K key : bucket.keySet()) {
+                    V value = bucket.get(key);
+                    int newIndex = Math.floorMod(key.hashCode(), newCapacity);
+                    if (newBuckets[newIndex] == null) {
+                        newBuckets[newIndex] = new ArrayMap<>();
+                    }
+                    newBuckets[newIndex].put(key, value);  // 重新插入到新桶
+                }
+            }
+        }
+        buckets = newBuckets;  // 更新桶数组
     }
 
     /* Returns the number of key-value mappings in this map. */
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return this.size;
+        // throw new UnsupportedOperationException();
     }
 
     //////////////// EVERYTHING BELOW THIS LINE IS OPTIONAL ////////////////
@@ -73,7 +109,14 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
     /* Returns a Set view of the keys contained in this map. */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keys = new HashSet<>();
+        for (ArrayMap<K, V> bucket : buckets) {
+            if (bucket != null) {
+                keys.addAll(bucket.keySet());
+            }
+        }
+        return keys;
+        // throw new UnsupportedOperationException();
     }
 
     /* Removes the mapping for the specified key from this map if exists.
@@ -81,7 +124,16 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * UnsupportedOperationException. */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        int index = hash(key);
+        if (buckets[index] == null) {
+            return null;
+        }
+        V value = buckets[index].remove(key);
+        if (value != null) {
+            size--;
+        }
+        return value;
+        // throw new UnsupportedOperationException();
     }
 
     /* Removes the entry for the specified key only if it is currently mapped to
@@ -89,11 +141,51 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
      * throw an UnsupportedOperationException.*/
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        int index = hash(key);
+        if (buckets[index] == null) {
+            return null;  // 如果桶为空，返回 null
+        }
+        V currentValue = buckets[index].get(key);
+        if (currentValue != null && currentValue.equals(value)) {
+            return remove(key);  // 如果值匹配，调用 remove 方法
+        }
+        return null;  // 否则返回 null
+        // throw new UnsupportedOperationException();
     }
 
     @Override
     public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
+        return new Iterator<K>() {
+            private int bucketIndex = 0;  // 当前桶索引
+            private Iterator<K> currentIterator = getNextIterator();  // 当前桶的迭代器
+
+            private Iterator<K> getNextIterator() {
+                while (bucketIndex < buckets.length) {
+                    if (buckets[bucketIndex] != null) {
+                        return buckets[bucketIndex].keySet().iterator();  // 返回当前桶的键迭代器
+                    }
+                    bucketIndex++;
+                }
+                return null;  // 如果没有更多桶，返回 null
+            }
+
+            @Override
+            public boolean hasNext() {
+                while (currentIterator == null || !currentIterator.hasNext()) {
+                    bucketIndex++;
+                    currentIterator = getNextIterator();  // 获取下一个桶的迭代器
+                }
+                return currentIterator != null && currentIterator.hasNext();
+            }
+
+            @Override
+            public K next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();  // 如果没有下一个元素，抛出异常
+                }
+                return currentIterator.next();  // 返回下一个元素
+            }
+        };
+        // throw new UnsupportedOperationException();
     }
 }
